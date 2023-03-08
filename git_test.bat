98ef4c1f4f309fba7a078de6c4c4a42fb3fbeb8f
axosecurity <sjsojib722@gmail.com>
2023-03-08 17:53:04 +0600

    Adding notes - Wed 03-08-2023 17:53:04.29

--- git_test.bat
+++ git_test.bat
@@ -1,4 +1,3 @@
-
 @echo off
 setlocal enabledelayedexpansion
 
@@ -6,26 +5,10 @@ set cmd=nslookup facebook.com
 echo Running command: %cmd%
 echo.
 
-for /f "tokens=* delims=" %%i in ('%cmd%') do (
-    set "output=!output!%%i\n"
-)
+(for /f "tokens=* delims=" %%i in ('%cmd%') do (
+    set "output=%%i"
+    echo ^<span style="color: green"^>!output!^</span^>
+)) > output.txt
 
-echo Output of %cmd% :
-
-echo !output!
-
-
-set cmd2=git remote add origin https://github.com/%github_username%/%repo_name%.git
-echo Running command: %cmd2%
-echo.
-
-for /f "delims=" %%i in ('%cmd2%') do (
-    set result=%%i
-    echo !result!
-)
-
-for /f "tokens=* delims=" %%i in ('%cmd%') do (
-    set "output=!output!%%i\n"
-)
-
-
+echo Output of %cmd%:
+type output.txt
